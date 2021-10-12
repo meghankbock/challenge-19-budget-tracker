366f77c76a10244941ebbedf26cf44ef3a2930ee
Meghan Bock <meghan.bock@gmail.com>
2021-10-12 04:13:52 -0500

    update service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,6 +16,21 @@ const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cahce : ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+})
+
 self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
@@ -24,3 +39,21 @@ self.addEventListener('install', function(e) {
         })
     )
 });
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function, (keyList) {
+            let cacheKeepList = keyList.filter(function (key) {
+                reutrn key.indexOf(APP_PREFIX);
+            })
+            cacheKeepList.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeepList.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i] );
+                    return caches.delete(keyList[i]);
+                }
+            }))
+        })
+    )
+})
