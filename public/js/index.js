1808aa045c55e5644969fcdca07c129ce36961ec
Meghan Bock <meghan.bock@gmail.com>
2021-10-12 09:39:02 -0500

    heroku troubleshooting

--- public/js/index.js
+++ public/js/index.js
@@ -17,46 +17,56 @@ fetch("/api/transaction")
 
 function populateTotal() {
   // reduce transaction amounts to a single total value
-  let total = transactions.reduce((total, t) => {
-    return total + parseInt(t.value);
-  }, 0);
-
-  let totalEl = document.querySelector("#total");
-  totalEl.textContent = total;
+  if (transactions) {
+    let total = transactions.reduce((total, t) => {
+      return total + parseInt(t.value);
+    }, 0);
+  
+    let totalEl = document.querySelector("#total");
+    totalEl.textContent = total;
+  }
+  return 0;
 }
 
 function populateTable() {
   let tbody = document.querySelector("#tbody");
   tbody.innerHTML = "";
 
-  transactions.forEach(transaction => {
-    // create and populate a table row
-    let tr = document.createElement("tr");
-    tr.innerHTML = `
-      <td>${transaction.name}</td>
-      <td>${transaction.value}</td>
-    `;
-
-    tbody.appendChild(tr);
-  });
+  if(transactions) {
+    transactions.forEach(transaction => {
+      // create and populate a table row
+      let tr = document.createElement("tr");
+      tr.innerHTML = `
+        <td>${transaction.name}</td>
+        <td>${transaction.value}</td>
+      `;
+  
+      tbody.appendChild(tr);
+    });
+  }
 }
 
 function populateChart() {
   // copy array and reverse it
-  let reversed = transactions.slice().reverse();
-  let sum = 0;
+  let labels = '';
+  let data = 0;
+
+  if (transactions) {
+    let reversed = transactions.slice().reverse();
+    let sum = 0;
 
   // create date labels for chart
-  let labels = reversed.map(t => {
+    labels = reversed.map(t => {
     let date = new Date(t.date);
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   });
 
   // create incremental values for chart
-  let data = reversed.map(t => {
+    data = reversed.map(t => { 
     sum += parseInt(t.value);
     return sum;
   });
+  }
 
   // remove old chart if it exists
   if (myChart) {
